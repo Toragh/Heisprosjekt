24f5fbaa656c6317588944f79e922dd86bc20bc9
Tora Haga <toragh@stud.ntnu.no>
2018-03-02 08:16:33 +0100

    timer updated

---
+++ timer.c
@@ -0,0 +1,7 @@
+#include "timer.h"
+#include "io.h"
+#include "channels.h"
+
+#include <stdio.h>
+#include <time.h>
+
---
+++ timer.h
@@ -0,0 +1,10 @@
+#ifndef __INCLUDE_IO_H__
+#define __INCLUDE_IO_H__
+
+
+
+void timer_function_3sec(void);
+
+
+
+#endif // #ifndef __INCLUDE_IO_H__
